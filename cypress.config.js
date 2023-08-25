269edc1a053b4aa7816635aa4f3ccf3f9cb81b24
SaraCoelhoCosta <costa.saracoelho@gmail.com>
2023-08-25 16:46:01 -0300

    Atualizando as dependências do projeto

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationpratice.com.br', // Site testado
-    defaultCommandTimeout: 7000, // Tempo entre cada ação
+    defaultCommandTimeout: 9000, // Tempo entre cada ação
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
